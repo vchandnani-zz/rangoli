5f506d9ef8041110fe95091aa0bf088c04de574a
Vishal Chandnani <vschandnani@gmail.com>
2015-02-12 00:24:23 -0500

    [VC] Setup dev, test and production databases
    - Added database configuration file
    - Added migration to create elephants table
    - Added .gitignore file to ignore SQLite DB files

---
+++ db/migrate/20150212040639_create_elephants_table.rb
@@ -0,0 +1,19 @@
+class CreateElephantsTable < ActiveRecord::Migration
+
+  def up
+    create_table  :elephants do |t|
+      t.text      :name
+      t.text      :rider
+      t.text      :passengers
+      t.timestamps
+    end
+    add_index :elephants, :name
+    add_index :elephants, :rider
+    add_index :elephants, :passengers
+  end
+
+  def down
+    drop_table :elephants
+  end
+
+end
