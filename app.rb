33094475076df534382a66fc3819cc39421a90bf
Vishal Chandnani <vschandnani@gmail.com>
2015-02-15 22:16:23 -0500

    [VC] Added route/service and tests to create elephants

--- app.rb
+++ app.rb
@@ -5,3 +5,16 @@ require './models/elephant'
 get '/' do
   "Hello World!"
 end
+
+post '/elephant' do
+  content_type :json
+  params = JSON.parse(request.body.read)
+  results = {}
+  e = Elephant.create( :name => params["name"],
+                       :rider => params["rider"],
+                       :passengers => params["passengers"] )
+    if e.id.nil?
+      halt 400, { error: e.errors }.to_json
+    end
+  return results.to_json
+end
--- test/app_test.rb
+++ test/app_test.rb
@@ -13,4 +13,13 @@ class AppTest < Minitest::Test
     assert_equal 'Hello World!', last_response.body
   end
 
+  def test_create_elephant
+		expected_results = {}
+		e1 = { :name => "rangoli", :rider => "vinny", :passengers => "bobby" }
+		post '/elephant', e1.to_json
+		assert_equal 200, last_response.status
+		results = JSON.parse(last_response.body)
+		assert_equal expected_results, results
+	end
+
 end
