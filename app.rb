d8c53df20fbe550e709a78cd5ff1f7aadf853b81
Vishal Chandnani <vschandnani@gmail.com>
2015-02-15 22:46:43 -0500

    [VC] Added route/service and tests to read elephants

--- app.rb
+++ app.rb
@@ -13,8 +13,20 @@ post '/elephant' do
   e = Elephant.create( :name => params["name"],
                        :rider => params["rider"],
                        :passengers => params["passengers"] )
-    if e.id.nil?
-      halt 400, { error: e.errors }.to_json
-    end
+  if e.id.nil?
+    halt 400, { error: e.errors }.to_json
+  end
   return results.to_json
+end
+
+get '/elephants' do
+  content_type :json
+	results = {}
+	e = Elephant.all
+	if e
+		results = e
+	else
+		halt 403, {error: {elephants: ["not found."]}}.to_json
+	end
+	return results.to_json
 end
--- test/app_test.rb
+++ test/app_test.rb
@@ -8,6 +8,10 @@ class AppTest < Minitest::Test
     Sinatra::Application
   end
 
+  def setup
+    Elephant.destroy_all
+  end
+
   def test_default_route
     get '/'
     assert_equal 'Hello World!', last_response.body
@@ -20,6 +24,32 @@ class AppTest < Minitest::Test
 		assert_equal 200, last_response.status
 		results = JSON.parse(last_response.body)
 		assert_equal expected_results, results
+	end
+
+  def test_create_elephant_with_duplicate_name
+		e1 = { :name => "rangoli", :rider => "vinny", :passengers => "bobby" }
+		post '/elephant', e1.to_json
+		post '/elephant', e1.to_json
+		assert_equal 400, last_response.status
+		results = JSON.parse(last_response.body)
+		assert_equal "is not unique.", results["error"]["name"][0]
+	end
+
+	def test_read_elephants
+		e1 = { :name => "rangoli", :rider => "vinny", :passengers => "bobby" }
+		post '/elephant', e1.to_json
+		e2 = { :name => "rangoli2", :rider => "vinny2", :passengers => "bobby2" }
+		post '/elephant', e2.to_json
+		get '/elephants'
+		assert_equal 200, last_response.status
+		results = JSON.parse(last_response.body)
+		assert_equal 2, results.size
+		assert_equal "rangoli", results[0]["name"]
+		assert_equal "vinny", results[0]["rider"]
+		assert_equal "bobby", results[0]["passengers"]
+		assert_equal "rangoli2", results[1]["name"]
+		assert_equal "vinny2", results[1]["rider"]
+		assert_equal "bobby2", results[1]["passengers"]
 	end
 
 end
