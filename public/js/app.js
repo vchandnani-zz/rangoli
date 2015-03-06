da6b3805da96bfe01c55796428525d533d0b1599
Vishal Chandnani <vschandnani@gmail.com>
2015-03-06 14:48:12 -0500

    [VC] AngularJS: Display all elephants on the entry page
    - Removed "loadData" function
    - Called $http.get('/elephants')
    - Removed "Load Elephants" button
    - Added table to display elephants

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,8 @@ var app = angular.module('myApp', []);
 
 app.controller('MyCtrl', function($scope, $http) {
 
-  $scope.loadData = function () {
-    $http.get('/elephants').success(function(data) {
-      $scope.elephants = data;
-      console.log(JSON.stringify($scope.elephants));
-    });
-  };
+  $http.get('/elephants').success(function(data) {
+    $scope.elephants = data;
+  });
 
 });
