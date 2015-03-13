cd927f647f8b370bad874acc1fbe0d120abc4a69
Vishal Chandnani <vschandnani@gmail.com>
2015-03-12 22:43:20 -0400

    [VC] Sinatra: Updated script with test curl commands
    - Renamed CRUD service to "elephants"
    - Update elephant by valid/invalid ID
    - Delete elephant by valid/invalid ID

--- scripts/curls.sh
+++ scripts/curls.sh
@@ -1,40 +1,55 @@
 #!/bin/sh
 
+# IMPORTANT:
+# TEST 1 creates 2 elephants with IDs 92 and 93 respectively.
+# TEST 6 updates the elephant with ID = 92.
+# TEST 9 deletes the elephant with ID = 93.
+# If the contents of the development database: db/rangoli_dev.sqlite3
+# change, the update and delete tests need to be changed accordingly.
+
 clear;
 
 echo "TEST 1: create elephants with valid name, rider and passengers"
-curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST -d '{ "name":"rangoli", "rider":"vinny", "passengers":"bobby" }' http://localhost:4567/elephant
-curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST -d '{ "name":"rangoli2", "rider":"vinny2", "passengers":"bobby2" }' http://localhost:4567/elephant
+curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST -d '{ "name":"rangoli", "rider":"vinny", "passengers":"bobby" }' http://localhost:4567/elephants
+curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST -d '{ "name":"rangoli2", "rider":"vinny2", "passengers":"bobby2" }' http://localhost:4567/elephants
 echo
 
 echo "TEST 2: create elephant with same name, rider and passengers"
-curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST -d '{ "name":"rangoli", "rider":"vinny", "passengers":"bobby" }' http://localhost:4567/elephant
+curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST -d '{ "name":"rangoli", "rider":"vinny", "passengers":"bobby" }' http://localhost:4567/elephants
 echo
 
 echo "TEST 3: read all elephants"
 curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X GET http://localhost:4567/elephants
 echo
 
-echo "TEST 4: update elephant by name"
-curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X PUT -d '{ "name":"rangoli", "rider":"vinny2", "passengers":"bobby2" }' http://localhost:4567/elephant
+echo "TEST 4: read elephant by valid ID"
+curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X GET http://localhost:4567/elephants/92
 echo
 
-echo "TEST 5: read all elephants"
+echo "TEST 5: read elephant by invalid ID"
+curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X GET http://localhost:4567/elephants/bad_id
+echo
+
+echo "TEST 6: update elephant by valid ID"
+curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X PUT -d '{ "name":"rangoli", "rider":"vinny2", "passengers":"bobby2" }' http://localhost:4567/elephants/92
+echo
+
+echo "TEST 7: read all elephants"
 curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X GET http://localhost:4567/elephants
 echo
 
-echo "TEST 6: update elephant by invalid name"
-curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X PUT -d '{ "name":"no_rangoli", "rider":"vinny2", "passengers":"bobby2" }' http://localhost:4567/elephant
+echo "TEST 8: update elephant by invalid ID"
+curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X PUT -d '{ "name":"no_rangoli", "rider":"vinny2", "passengers":"bobby2" }' http://localhost:4567/elephants/bad_id
 echo
 
-echo "TEST 7: delete elephant by valid name"
-curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X DELETE http://localhost:4567/elephant/rangoli
+echo "TEST 9: delete elephant by valid ID"
+curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X DELETE http://localhost:4567/elephants/93
 echo
 
-echo "TEST 8: read all elephants"
+echo "TEST 10: read all elephants"
 curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X GET http://localhost:4567/elephants
 echo
 
-echo "TEST 9: delete elephant by invalid name"
-curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X DELETE http://localhost:4567/elephant/no_rangoli
+echo "TEST 11: delete elephant by invalid ID"
+curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X DELETE http://localhost:4567/elephants/bad_id
 echo
