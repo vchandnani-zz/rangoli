a1927490513aed4424e969b5f282650207c5aff5
Vishal Chandnani <vschandnani@gmail.com>
2015-03-14 15:50:23 -0400

    [VC] AngularJS: Added factory to CRUD elephants
    - Added factory to make HTTP requests
    - Renamed controllers (List, Add and Edit)
    - Controllers: Added success and error functions

--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,89 @@
-var app = angular.module('elephantsApp', []).
-  config(function ($routeProvider, $locationProvider) {
-    $routeProvider.
-      when("/", {templateUrl: "/views/list.html"}).
-      when("/new", {templateUrl: "/views/edit.html", controller: "elephantController"}).
-      when("/edit/:id", {templateUrl: "/views/edit.html", controller: "elephantsController"}).
-      otherwise({redirectTo: "/"});
-  });
+var app = angular.module('elephantsApp', []);
 
-function elephantsAppController($scope, $http, $window) {
-  $http.get('/elephants').success(function(data) {
-    $scope.elephants = data;
-  });
-  $scope.destroy = function(elephant_id) {
+app.config(function ($routeProvider, $locationProvider) {
+  $routeProvider.
+    when("/", {templateUrl: "/views/list.html"}).
+    when("/add", {templateUrl: "/views/edit.html", controller: "elephantsAddController"}).
+    when("/edit/:id", {templateUrl: "/views/edit.html", controller: "elephantsEditController"}).
+    otherwise({redirectTo: "/"});
+});
+
+app.factory('elephantFactory', ['$http', function($http) {
+  var urlBase = '/elephants';
+  var elephantFactory = {};
+
+  elephantFactory.getElephants = function () {
+    return $http.get(urlBase);
+  };
+
+  elephantFactory.addElephant = function (elephant) {
+    return $http.post(urlBase, elephant);
+  };
+
+  elephantFactory.updateElephant = function (elephant) {
+    return $http.put(urlBase + '/' + elephant.id, elephant)
+  };
+
+  elephantFactory.deleteElephant = function (id) {
+    return $http.delete(urlBase + '/' + id);
+  };
+
+  return elephantFactory;
+}]);
+
+function elephantsListController($scope, elephantFactory, $window) {
+  $scope.status;
+  $scope.elephants;
+
+  getElephants();
+
+  function getElephants() {
+    elephantFactory.getElephants()
+      .success(function (elephants) {
+        $scope.elephants = elephants;
+      })
+      .error(function (error) {
+        $scope.status = 'Unable to load elephants: ' + error.message;
+      });
+  }
+
+  $scope.deleteElephant = function(elephant_id) {
     if (confirm("Are you sure?")) {
-      $http.delete('/elephants/' + elephant_id);
-      $window.location.href = "/";
+      elephantFactory.deleteElephant(elephant_id)
+        .success(function () {
+          $window.location.href = "/";
+        })
+        .error(function (error) {
+          $scope.status = 'Unable to delete elephant: ' + error.message;
+        });
     }
   }
 }
 
-function elephantController($scope, $http, $window) {
-  $scope.title = "New Elephant";
+function elephantsAddController($scope, elephantFactory, $window) {
+  $scope.title = "Add Elephant";
   $scope.elephant = {name: "", rider: "", passengers: ""};
   $scope.save = function() {
-    $http.post('/elephants', $scope.elephant);
-    $window.location.href = "/";
+    elephantFactory.addElephant($scope.elephant)
+      .success(function () {
+        $window.location.href = "/";
+      }).
+      error(function(error) {
+        $scope.status = 'Unable to add elephant: ' + error.message;
+      });
   }
 }
 
-function elephantsController($scope, $routeParams, $http, $location) {
+function elephantsEditController($scope, $routeParams, elephantFactory, $location) {
   $scope.title = "Edit Elephant";
   $scope.elephant = $scope.elephants[$routeParams.id];
   $scope.save = function() {
-    $http.put('/elephants', $scope.elephant);
-    $location.path("/");
+    elephantFactory.updateElephant($scope.elephant)
+      .success(function () {
+        $location.path("/");
+      })
+      .error(function (error) {
+        $scope.status = 'Unable to update elephant: ' + error.message;
+      });
   }
 }
