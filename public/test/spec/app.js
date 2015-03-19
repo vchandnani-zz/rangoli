5de287b266a96ce8eec400f7d3cac0a41a8da085
Vishal Chandnani <vschandnani@gmail.com>
2015-03-18 22:16:22 -0400

    [VC] Added unit tests for elephant factory
    - Added unit test to add elephants
    - Added unit test to update elephants
    - Added unit test to delete elephants

--- public/test/spec/app.js
+++ public/test/spec/app.js
@@ -16,11 +16,40 @@ describe('elephant factory read elephants', function() {
     httpMock.verifyNoOutstandingRequest();
   });
 
-  it('should get the list of elephants', function() {
-
+  it('should get all elephants', function() {
     var expectedData = { name: 'rangoli', rider: 'vinny', passengers: 'bobby' };
     httpMock.expectGET('/elephants').respond(expectedData);
     factory.getElephants().then( function (response) {
+      expect(response.data).toEqual(expectedData)
+    });
+    httpMock.flush();
+  });
+
+  it('should add elephant', function() {
+    var expectedData = {};
+    var elephantData = { name: 'rangoli', rider: 'vinny', passengers: 'bobby' };
+    httpMock.expectPOST('/elephants', elephantData).respond(expectedData);
+    factory.addElephant(elephantData).then( function (response) {
+      expect(response.data).toEqual(expectedData)
+    });
+    httpMock.flush();
+  });
+
+  it('should update elephant', function() {
+    var expectedData = {};
+    var elephantData = { id: 3, name: 'rangoli', rider: 'vinny', passengers: 'bobby' };
+    httpMock.expectPUT('/elephants/3', elephantData).respond(expectedData);
+    factory.updateElephant(elephantData).then( function (response) {
+      expect(response.data).toEqual(expectedData)
+    });
+    httpMock.flush();
+  });
+
+  it('should delete elephant', function() {
+    var expectedData = {};
+    var elephantData = { id: 3, name: 'rangoli', rider: 'vinny', passengers: 'bobby' };
+    httpMock.expectDELETE('/elephants/3').respond(expectedData);
+    factory.deleteElephant(elephantData.id).then( function (response) {
       expect(response.data).toEqual(expectedData)
     });
     httpMock.flush();
