f28f7d6a4c8d8cb2ca729f65fac33ec4bfcdfa85
Vishal Chandnani <vschandnani@gmail.com>
2015-06-24 17:22:48 -0400

    [VC] AngularJS: End-to-End tests for elephants
    - Add: Verify added elephant attributes
    - Delete: Verify deleted elephant is not in list of elephants

--- public/test/e2e/crud.js
+++ public/test/e2e/crud.js
@@ -17,15 +17,30 @@ describe('E2E tests: add elephants', function() {
 		var elephants = element.all(by.repeater('elephant in elephants'));
     expect(elephants.count()).toEqual(10);
     element(by.linkText('Add')).click();
-		element(by.model('elephant.name')).sendKeys('my favorite name');
-		element(by.model('elephant.rider')).sendKeys('my favorite rider');
-		element(by.model('elephant.passengers')).sendKeys('my favorite passengers');
+		element(by.model('elephant.name')).sendKeys('vinny75');
+		element(by.model('elephant.rider')).sendKeys('rider75');
+		element(by.model('elephant.passengers')).sendKeys('pass75');
     element(by.buttonText("Save")).click();
 		browser.driver.sleep(5);
     browser.waitForAngular();
 		elephants = element.all(by.repeater('elephant in elephants'));
     expect(elephants.count()).toEqual(11);
-    var elephant_to_delete = 'my favorite name';
+		var added_name = "vinny75";
+    var elephant_attributes = [];
+    element.all(by.repeater('elephant in elephants')).filter(function(row) {
+      return row.element(by.css('.ng-binding')).getText().then(function(name) {
+        return name === added_name; 
+      });
+    })
+    .get(0)
+		.getText()
+		.then(function(text) {
+			elephant_attributes = text.split(" ");
+			expect(elephant_attributes[0]).toEqual('vinny75');
+			expect(elephant_attributes[1]).toEqual('rider75');
+			expect(elephant_attributes[2]).toEqual('pass75');
+		});
+    var elephant_to_delete = 'vinny75';
     element.all(by.repeater('elephant in elephants')).filter(function(row) {
       return row.element(by.css('.ng-binding')).getText().then(function(name) {
         return name === elephant_to_delete; 
@@ -38,6 +53,15 @@ describe('E2E tests: add elephants', function() {
     alertDialog.accept();
 		elephants = element.all(by.repeater('elephant in elephants'));
     expect(elephants.count()).toEqual(10);
+    var deleted_name = 'vinny75';
+    element.all(by.repeater('elephant in elephants')).filter(function(row) {
+      return row.element(by.css('.ng-binding')).getText().then(function(name) {
+        return name === deleted_name; 
+      });
+    })
+		.then(function(items) {
+		  expect(items.length).toBe(0);
+		});
 	});
 
 	it('should update elephant in list of elephants', function() {
